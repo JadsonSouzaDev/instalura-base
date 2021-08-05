9ddfe2b43f03a4f914827f04649060d72d8d09a3
Jadson Souza <jadson.souza@ifood.com.br>
2021-08-05 11:21:37 -0300

    feat: add reset css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Menu from "../src/components/commons/Menu";
+import Footer from "../src/components/commons/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flex: "1",
+        flexWrap: "wrap",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <Menu />
+      <Footer />
     </div>
   );
 }
